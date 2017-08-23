98decc3e3adb573ea1c8aeec26c38a4ea152e9d2
Thomas Edwards <thomas.edwards@gmail.com>
2017-08-23 16:59:10 +0100

    Refactors scripts, adds BBC News parser

--- scripts.js
+++ scripts.js
@@ -1,22 +1,37 @@
+// Try meta property
 var metaTags = document.getElementsByTagName('meta');
-
 for (var i = 0, len = metaTags.length; i < len; i++) {
   if (metaTags[i].getAttribute('property') === 'article:published_time') {
-    var articleDate = new Date(metaTags[i].getAttribute('content'));
-
-    var dateDiff = Date.now() - articleDate.getTime();
-
-    if (dateDiff < 0) {
-      // Date is in the future, wtf?
-      break;
-    }
-
-    if (dateDiff > (7 * 24 * 60 * 60 * 1000)) { // 7 days
-
-      var warningDiv = document.createElement('div');
-      warningDiv.classList.add('is-it-relevant-week-old');
-      warningDiv.innerHTML = 'Is It Relevant? This article is over a week old';
-      document.body.prepend(warningDiv);
-    }
+    var articleDate = metaTags[i].getAttribute('content');
   }
+}
+
+// Try application/ld+json
+var scriptTags = document.getElementsByTagName('script');
+for (var i = 0, len = scriptTags.length; i < len; i++) {
+  if (scriptTags[i].getAttribute('type') === 'application/ld+json') {
+    var linkedData = JSON.parse(scriptTags[i].innerHTML);
+    articleDate = linkedData.datePublished;
+  }
+}
+
+var articleDate = new Date(articleDate);
+
+if (! articleDate) {
+  throw new Error('Cannot parse date from page');
+}
+
+var dateDiff = Date.now() - articleDate.getTime();
+
+if (dateDiff < 0) {
+  // Date is in the future, wtf?
+  throw new Error('Date is in the future');
+}
+
+if (dateDiff > (7 * 24 * 60 * 60 * 1000)) { // 7 days
+
+  var warningDiv = document.createElement('div');
+  warningDiv.classList.add('is-it-relevant-week-old');
+  warningDiv.innerHTML = 'Is It Relevant? This article is over a week old';
+  document.body.prepend(warningDiv);
 }
